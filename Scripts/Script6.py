19a7cfde6cd8c309c25f33ed1b99213ea2817b15
Giovanni Benedet Rodrigues <gi-benedet@hotmail.com>
2024-09-25 12:38:01 -0300

    EX_6_parte_6
    Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.

---
+++ Scripts/Script6.py
@@ -0,0 +1,7 @@
+#Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+
+nome = str(input('digite seu nome completo: ')).title().strip()
+
+separado = nome.split()
+
+print('seu  primeiro nome é {} e seu ultimo nome é {}.'.format(separado[0] , separado[2]))
